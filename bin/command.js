ea7527c2c0573b7377e1f3504bea152db622defb
charlesliu_it <charlesliu_it@163.com>
2018-12-23 23:24:33 +0800

    增加功能

--- bin/command.js
+++ bin/command.js
@@ -1,15 +1,54 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-const cmd     = require('node-cmd')
+const fs        = require('fs')
+const cmd       = require('node-cmd')
+const ora       = require('ora');
+const chalk     = require('chalk');
+const symbols   = require('log-symbols');
+const program   = require('commander')
+const inquirer  = require('inquirer');
+const download  = require('download-git-repo');
+
 program.version("0.0.1")
+
 program.command("init <ProjectName>")
     .description("create a new Project")
     .action(function(projectName) {
-        console.info(`init Project ${projectName}`)
-        cmd.get(
-            `node ./init.js`
-        )
+        if(!fs.existsSync(projectName)) {
+            inquirer.prompt([
+                { type: 'input', name: 'useRedux', message: 'use redux(y/n):'},
+            ]).then((options) => {
+                const spinner = ora('downloading...');
+                if(options.useRedux === 'y' || options.useRedux === 'yes' || options.useRedux === '是'){
+                    spinner.start()
+                    download("https://github.com/zhenliliu/bee-cli",projectName, {clone: false}, (error) => {
+                        if(error) {
+                            console.log('error', error)
+                            spinner.fail('下载失败')
+                        }
+                    })
+                } else if (options.useRedux === 'n' || options.useRedux === 'no' || options.useRedux === '否'){
+                    console.log('不使用redux', options)
+                } else {
+                    console.log(symbols.warning, chalk.red(`请输入正确命令！`))
+                }
+            })
+
+        } else {
+            console.log(symbols.warning, chalk.red(`文件${projectName}已经存在`)); 
+        }
+        // if(fs.stat(projectName))
+        // const spinner = ora('正在下载模板...');
+        // spinner.start();
+        // setTimeout(() => {
+        //     // spinner.fail();
+        //     spinner.succeed();
+        //     console.log(symbols.success, chalk.green('项目创建成功'));
+        //          
+        // },3000)
+        // cmd.get(
+        //     `node ./init.js`
+        // )
     })
 program.parse(process.argv)
 
